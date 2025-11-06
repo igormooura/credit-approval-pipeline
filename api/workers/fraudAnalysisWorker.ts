5f4a22e588b18ce0d131d660a155a209a5eecbd8
Igor Moura <igor.oliveira.moura@gmail.com>
2025-11-06 13:19:34 -0300

    feat: fraud worker

---
+++ api/workers/fraudAnalysisWorker.ts
@@ -0,0 +1,24 @@
+import { consumeQueue } from "../queues/rabbitmq"
+
+export const fraudAnalysisWorker = async () =>{
+    try {
+        const credit_analysis_queue = process.env.CREDIT_ANALYSIS_QUEUE
+
+        if(credit_analysis_queue){
+            await consumeQueue(credit_analysis_queue, async(msg)=>{
+                const credit_content = JSON.parse(msg.content.toString());
+                const proposalId = credit_content.proposalId;
+
+                if (!proposalId) throw new Error("There's no proposal id")
+
+                //await fraudAnalysisService(proposalId)
+
+                await new Promise(resolve => setTimeout(resolve, 500))
+            })  
+        } else { 
+            console.log("credit_analysis_queue not set")
+        }
+    } catch (error: any) { 
+        console.log(error)
+    }
+}
