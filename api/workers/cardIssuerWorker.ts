d58e18c674c7cb7d35cefbaef997607c0f32b584
Igor Moura <igor.oliveira.moura@gmail.com>
2026-01-27 18:18:38 -0300

    fix: wrong parameter to card issuer

--- api/workers/cardIssuerWorker.ts
+++ api/workers/cardIssuerWorker.ts
@@ -1,22 +1,20 @@
-import { bindQueueToExchange, consumeQueue } from "../queues/rabbitmq";
+import { bindQueueToExchange, consumeQueue } from "../queues/rabbitmq"; //
 
 const handleCardIssue = async (msg: any) => {
-    try {
-        const content = JSON.parse(msg.content.toString());
-        const { fullName, cardType } = content;
 
-        if (!fullName || !cardType) {
-            console.warn("incomplete data for card issuance:", content);
-            return; //  without error so the message is removed from the queue ack and doesn't crash
-        }
+    const content = JSON.parse(msg.content.toString());
 
-        if (cardType === 'PLATINUM') {
-            console.log(`${fullName} platinum card sent to production`);
-        } else {
-            console.log(`Generating PLASTIC card for ${fullName}`);
-        }
-    } catch (error) {
-        console.error("Fatal error processing card:", error);
+    const { customerName, cardType } = content;
+    const fullName = customerName; 
+
+    if (!fullName || !cardType) {
+        throw new Error(`incomplete data for card issuance: ${JSON.stringify(content, null, 2)}`);
+    }
+
+    if (cardType === 'PLATINUM') {
+        console.log(`${fullName} platinum card sent to production`);
+    } else {
+        console.log(`generating PLASTIC card for ${fullName}`);
     }
 };
 
@@ -28,9 +26,10 @@ export const cardIssuerWorker = async () => {
         if(!exchangeName || !cardIssuerQueue) throw new Error("Card queue or Exchange name not defined");
 
         await bindQueueToExchange(cardIssuerQueue, exchangeName);
+
         await consumeQueue(cardIssuerQueue, handleCardIssue);
 
     } catch (error) {
-        console.error("Failed to initialize cardIssuerWorker:", error);
+        console.error("Error initializing Card Issuer Worker:", error);
     }
 };
